caf6099b830d23dbc5bbb7a3155dfd474f506549
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-24 18:41:00 +0900

    Tweetモデルのカラム追加

---
+++ db/migrate/20201124091030_create_tweets.rb
@@ -0,0 +1,19 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.string  :task_1
+      t.text    :detalis_1
+      t.integer :start_time_1_id
+      t.integer :end_time_1_id
+      t.string  :task_2
+      t.text    :detalis_2
+      t.integer :start_time_2_id
+      t.integer :end_time_2_id
+      t.string  :task_3
+      t.text    :detalis_3
+      t.integer :start_time_3_id
+      t.integer :end_time_3_id
+      t.timestamps
+    end
+  end
+end
