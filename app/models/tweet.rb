8bf97a61415fc06da0f75950b988e3161341d549
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-25 18:56:23 +0900

    StartMinuteモデルの作成とアソシエーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,12 @@
 class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :start_hour
+  belongs_to :start_minute
 
   validates :task_1, presence: true
   with_options numericality: { other_than: 1, message: "は「---」以外のものを選択してください"}  do
-    validates :start_time_1_id
-    validates :end_time_1_id
+    validates :start_hour_id
+    validates :start_minute_id
     validates :start_minutes_1_id
     validates :end_minutes_1_id
   end
---
+++ test/models/start_minute_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StartMinuteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
