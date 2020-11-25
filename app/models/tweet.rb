79f06675ea9ff52fcfaf68081dd7117b38f86e9a
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-25 18:45:21 +0900

    StartHourモデルの作成とアソシエーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,6 @@
 class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :timetable
-  belongs_to :minutestable
+  belongs_to :start_hour
 
   validates :task_1, presence: true
   with_options numericality: { other_than: 1, message: "は「---」以外のものを選択してください"}  do
---
+++ test/models/start_hour_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StartHourTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
