7a1f9f378ebe7669970cad4991b661da240b9b1f
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-25 19:25:37 +0900

    common_moduleモデルの削除

--- app/models/concerns/common_module.rb
+++
@@ -1,9 +0,0 @@
-module CommonModule
-  extend ActiveSupport::Concern
-
-  module ClassMethods
-    def column_symbolized_names(reject=[])
-      column_names.delete_if {|n| n.in?(reject)}.map(&:to_sym)
-    end
-  end
-end
