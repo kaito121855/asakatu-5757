2465ba184ed4dd25cb30bf9f0ff83673bae3c0e6
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-25 18:59:18 +0900

    EndHourモデルの作成とアソシエーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,12 +2,13 @@ class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :start_hour
   belongs_to :start_minute
+  belongs_to :end_hour
 
   validates :task_1, presence: true
   with_options numericality: { other_than: 1, message: "は「---」以外のものを選択してください"}  do
     validates :start_hour_id
     validates :start_minute_id
-    validates :start_minutes_1_id
+    validates :end_hour_id
     validates :end_minutes_1_id
   end
 
