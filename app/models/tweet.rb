2ba33dcb06dd66cc85dad1801f9a42935401c222
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-25 19:01:35 +0900

    EndMinuteモデルの作成とアソシエーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,13 +3,14 @@ class Tweet < ApplicationRecord
   belongs_to :start_hour
   belongs_to :start_minute
   belongs_to :end_hour
+  belongs_to :end_minute
 
   validates :task_1, presence: true
   with_options numericality: { other_than: 1, message: "は「---」以外のものを選択してください"}  do
     validates :start_hour_id
     validates :start_minute_id
     validates :end_hour_id
-    validates :end_minutes_1_id
+    validates :end_minute_id
   end
 
   include CommonModule
---
+++ test/models/end_minute_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EndMinuteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
