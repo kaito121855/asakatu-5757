c7c206f13775426245d7c378d90b98842159d51c
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-11-25 10:55:47 +0900

    Tweetモデルのバリデーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,12 @@ class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :timetable
   belongs_to :minutestable
+
+  validates :task_1, presence: true
+  with_options numericality: { other_than: 1, message: "は「---」以外のものを選択してください"}  do
+    validates :start_time_1_id
+    validates :end_time_1_id
+    validates :start_minutes_1_id
+    validates :end_minutes_1_id
+  end
 end
