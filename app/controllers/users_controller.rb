f333c1159c5674d6eaec03eda3d15ea1815a5cfe
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-12-01 18:22:28 +0900

    朝活タスクページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
     @tweets = user.tweets
+  end
+
+  def task
+    @user = User.find(params[:id])
+    @new = Time.current
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+      get 'task'
+    end
+  end
 end
