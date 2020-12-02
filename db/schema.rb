adf5fc6519019cc3d0addf53fe7ba2a890c42e5a
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-12-02 16:06:22 +0900

    Commentモデルのカラム追加
    ・user_id
    ・tweet_id
    ・text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_032157) do
+ActiveRecord::Schema.define(version: 2020_12_02_065755) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "task", null: false
