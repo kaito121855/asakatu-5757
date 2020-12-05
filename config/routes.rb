78461584c4a0dd2055417f8017d27ca8eeab596e
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-12-05 20:57:34 +0900

    お気に入り機能に関するルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,28 @@
 Rails.application.routes.draw do
+
+  # ルート先の設定
   root to: 'tweets#index'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-  resources :tweets do
-    resources :comments, only: :create
-  end
+
+  # ユーザー機能に関するルーティング
   resources :users, only: :show do
+    get :fovorites, on: :collection
     member do
       get :task, :following, :followers
     end
   end
+
+  # 投稿機能に関するルーティング
+  resources :tweets do
+    resources :favorites, only: [:create, :destroy]
+    resources :comments, only: :create
+  end
+
+  # ログイン機能に関するルーティング
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  # フォロー機能に関するルーティング
   resources :relationships, only: [:create, :destroy]
+  
 end
