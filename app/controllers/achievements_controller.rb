177bbdf7ba117a0968f2a6a88dd0c5f8f433984b
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-12-07 16:14:43 +0900

    達成機能に関するコントローラーの作成

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,2 +1,20 @@
 class AchievementsController < ApplicationController
+  before_action :set_tweet
+  before_action :authenticate_user!
+
+  def create
+    @favorite = current_user.favorites.create(tweet_id: params[:tweet_id])
+  end
+
+  def destroy
+    @favorite =current_user.favorites.find_by(tweet_id: @tweet.id)
+    @favorite.destroy
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+
 end
