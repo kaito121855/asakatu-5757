a0d21d42013f1b805f93a1db7debee2bab0d8e55
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-12-17 16:07:43 +0900

    ストロングパラメーターの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -42,7 +42,7 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:task, :detalis, :start_hour_id, :start_minute_id, :end_hour_id, :end_minute_id).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:task, :category_id, :detalis, :start_hour_id, :start_minute_id, :end_hour_id, :end_minute_id, :detalis).merge(user_id: current_user.id)
   end
 
   def set_tweet
