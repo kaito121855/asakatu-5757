11417478096a3cb81e0caa74a75af1c36748ea53
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2020-12-19 16:30:04 +0900

    categoriesコントローラーの各アクション設定

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,27 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @tweets = Tweet.includes(:user).order('tweets.created_at DESC')
+  end
+
+  def training
+    @tweets = Tweet.where(category_id: 2).order('tweets.created_at DESC')
+  end
+
+  def walking
+    @tweets = Tweet.where(category_id: 3).order('tweets.created_at DESC')
+  end
+
+  def study
+    @tweets = Tweet.where(category_id: 4).order('tweets.created_at DESC')
+  end
+
+  def health
+    @tweets = Tweet.where(category_id: 5).order('tweets.created_at DESC')
+  end
+
+  def housework
+    @tweets = Tweet.where(category_id: 6).order('tweets.created_at DESC')
+  end
+
+end
