4144bd2d2e946754d093e290402a0db0df3b8e30
kaito121855 <70756110+kaito121855@users.noreply.github.com>
2021-01-05 15:10:26 +0900

    Capistranoの修正#4

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :bundle_gemfile,  "server/Gemfile"
-
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
